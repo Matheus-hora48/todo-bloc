f21a1d0ade10721a923f326776494a08207e08b2
Matheus Hora <matheus11hora@gmail.com>
2024-12-03 11:57:49 -0300

    ✨ feat: adiciona validacao ao excluir

--- lib/src/features/todo/presentation/widgets/task_item_widget.dart
+++ lib/src/features/todo/presentation/widgets/task_item_widget.dart
@@ -18,15 +18,61 @@ class TaskItemWidget extends StatelessWidget {
         task.isCompleted ? Icons.check_circle : Icons.radio_button_unchecked,
         color: task.isCompleted ? Colors.green : null,
       ),
-      title: Text(task.title),
+      title: Text(
+        task.title,
+        style: TextStyle(
+          decoration: task.isCompleted ? TextDecoration.lineThrough : null,
+          color: task.isCompleted ? Colors.grey : null,
+        ),
+      ),
       subtitle: Visibility(
         visible: task.description?.isNotEmpty ?? false,
-        child: Text(task.description!),
+        child: Text(
+          task.description ?? '',
+          style: TextStyle(
+            decoration: task.isCompleted ? TextDecoration.lineThrough : null,
+            color: task.isCompleted ? Colors.grey : null,
+          ),
+        ),
       ),
       trailing: IconButton(
-        icon: const Icon(Icons.delete),
-        onPressed: onDelete,
+        icon: const CircleAvatar(
+          child: Icon(
+            Icons.delete,
+          ),
+        ),
+        onPressed: () {
+          _showDeleteConfirmationDialog(context);
+        },
       ),
+    );
+  }
+
+  void _showDeleteConfirmationDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Confirmar Exclusão'),
+          content:
+              const Text('Você tem certeza que deseja excluir esta tarefa?'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Cancelar'),
+            ),
+            TextButton(
+              onPressed: () {
+                onDelete();
+                Navigator.of(context).pop();
+              },
+              child: const Text('Excluir'),
+            ),
+          ],
+        );
+      },
     );
   }
 }
